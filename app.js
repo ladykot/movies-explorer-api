4b5c612d9f8789708356cf6ede0dabd44c77a162
Katya Akilina <katyaartline@gmail.com>
2023-05-26 00:43:14 +0300

    feat: add LOCAL_DB, cors origin

--- app.js
+++ app.js
@@ -11,15 +11,20 @@ const { requestLogger, errorLogger } = require('./midlewares/logger');
 const router = require('./routes/index');
 const limiter = require('./utils/limiter');
 
-const { DATA_BASE, NODE_ENV } = process.env;
+const { DATA_BASE, NODE_ENV, LOCAL_DB } = process.env;
 const { PORT = 3000 } = process.env;
 const app = express();
-mongoose.connect(NODE_ENV === 'production' ? DATA_BASE : 'mongodb://localhost:27017/moviesdb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  NODE_ENV === 'production' ? DATA_BASE : LOCAL_DB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://movies-explorer-ladykot.netlify.app',
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
